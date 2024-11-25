1ec097a3411c56526e5e0735b5a2c3b9a0d4c0cc
Kimathi-Mutegi <kimathimutegi186@gmail.com>
2024-11-25 10:58:37 +0300

    first commit

--- rentalcars_ecom/app/views.py
+++ rentalcars_ecom/app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from urllib import request
 
 # Create your views here.
 def home(request):
-    return render(request, "app/")
+    return render(request, "app/base.html")
--- rentalcars_ecom/rentalcars_ecom/settings.py
+++ rentalcars_ecom/rentalcars_ecom/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -57,7 +56,7 @@ ROOT_URLCONF = 'rentalcars_ecom.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
